cd9565655a40184f748b985520eb64b806a0b06e
sensuy1 <sultanbejsenbekov@gmail.com>
2023-11-21 17:22:17 +0600

    dz5

---
+++ app.js
@@ -0,0 +1,38 @@
+// let nums = [22,22,76,76,69,69,33,33,5]
+// let nums2 = []
+// for(let i = 0; i < nums.length; i++){
+//     if(nums2.includes(nums[i])){
+//     }else{
+//         nums2.push(nums[i])
+//     }
+// }
+// console.log(nums2)
+
+// function BooksMapFilter(bookArray) {
+//     let booksWithU = bookArray.filter(i => i.includes('у'))
+//     let otherBooks = bookArray.filter(i => !i.includes('у'))
+//     let booksWithUText = booksWithU.map(i => `${i} - книга с буквой 'у'`)
+//     console.log('Книги с буквой "у":', booksWithUText)
+//     console.log('\nОстальнве книги:', otherBooks)
+// }
+// let bookArray = ["улисс", "тень ветра", "война и мир", "убить пересмешнка",]
+// BooksMapFilter(bookArray)
+
+// const users = [
+//  {name:'Султан', age: 17},
+//  {name:'Дастан', age: 23},
+//  {name:'Тимур', age: 34},
+//  {name:'Найара', age: 18},
+//  {name:'Нурсултан', age: 19},
+//  {name:'Раяна', age: 25},
+//  {name:'Байнур', age: 11},
+//  {name:'Лейла', age: 7},
+// ]
+// let oldestPerson = users.reduce((oldest, current) =>
+// (current.age > oldest.age) ? current : oldest)
+// console.log(oldestPerson)
+// let youngPerson = users.reduce((young, current) =>
+// (current.age < young.age) ? current : young)
+// console.log(youngPerson)
+
+
